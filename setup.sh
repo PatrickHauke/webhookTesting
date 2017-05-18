6a5f75c14bb81db0abf3fd4abc844b564db42e08
PatrickHauke <patrick.hauke@outlook.com>
2017-05-18 18:21:57 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ git reset --hard origin/testing
 git clean -f  
 git pull  
 git checkout testing
-rm ../someData.zip
+#rm ../someData.zip
 echo 'should have deleted someZip old!'
 zip -r ../someData *
 
